56dee085063aaa455da8488e8211c66b23597ac4
thrillseeker <thomas.madl@gmail.com>
2015-08-16 15:09:24 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(name="pySeqSLAM",
       cmdclass={'test': PyTest},
       
       # TODO: List of packages that this one depends upon:   
-      install_requires=['scipy', 'numpy', 'matplotlib'],
+      install_requires=['scipy', 'numpy', 'matplotlib', 'Pillow'],
       # TODO: List executable scripts, provided by the package (this is just an example)
       entry_points={
         'console_scripts': 
